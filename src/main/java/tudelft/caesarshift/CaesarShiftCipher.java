86e5619b9e92d235ade2ba1f04d54295acfc6f0b
jmnetherlands <jurgenmuns@hotmail.com>
2018-11-11 09:37:52 +0100

    CaesarShift bugfixes: not appending each letter from original message and support for spaces

--- src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
+++ src/main/java/tudelft/caesarshift/CaesarShiftCipher.java
@@ -11,16 +11,19 @@ public class CaesarShiftCipher {
 
         for(int i = 0; i < length; i++){
             currentChar = message.charAt(i);
-           
-            sb.append(currentChar);
-            if (currentChar > 'z' || currentChar < 'a') {
-                return "invalid";
-            } else if ((char) (currentChar + shift) > 'z') {
-                currentChar = (char) (currentChar - 26);
-            } else if ((char) (currentChar + shift) < 'a'){
-                currentChar = (char) (currentChar + 26);
+
+            if (currentChar == ' ') {
+                sb.append(currentChar);
+            } else {
+                if (currentChar > 'z' || currentChar < 'a') {
+                    return "invalid";
+                } else if ((char) (currentChar + shift) > 'z') {
+                    currentChar = (char) (currentChar - 26);
+                } else if ((char) (currentChar + shift) < 'a') {
+                    currentChar = (char) (currentChar + 26);
+                }
+                sb.append((char) (currentChar + shift));
             }
-            sb.append((char) (currentChar + shift));
         }
 
         return sb.toString();
--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,26 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import tudelft.ghappy.GHappy;
 
+public class CaesarShiftCipherTest {
+    private CaesarShiftCipher caesarShiftCipher;
+
+    @BeforeEach
+    public void initialize() {
+        caesarShiftCipher = new CaesarShiftCipher();
+    }
+
+    // TODO should put each line of code into a single testmethod, but as this is only exercise code no need to spend time on it.
+    @Test
+    public void test() {
+        Assertions.assertEquals("def", caesarShiftCipher.CaesarShiftCipher("abc", 3));
+        Assertions.assertEquals("d ef", caesarShiftCipher.CaesarShiftCipher("a bc", 3));
+        Assertions.assertEquals("abd", caesarShiftCipher.CaesarShiftCipher("xya", 3));
+        Assertions.assertEquals("ihg", caesarShiftCipher.CaesarShiftCipher("fed", 3));
+        Assertions.assertEquals("xyz", caesarShiftCipher.CaesarShiftCipher("abc", -3));
+        Assertions.assertEquals("invalid", caesarShiftCipher.CaesarShiftCipher("abC", -3));
+    }
 }
