7c209ec6e8c8870c38f239740a31c2e906e9e687
jmnetherlands <jurgenmuns@hotmail.com>
2018-11-11 09:23:16 +0100

    GHappy fix and testclass

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,59 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    GHappy gHappy;
+
+    @BeforeEach
+    public void test() {
+        gHappy = new GHappy();
+    }
+
+    @Test
+    public void testNone() {
+        Assertions.assertEquals(true, gHappy.gHappy("abc"));
+    }
+
+    @Test
+    public void testDoubleG() {
+        Assertions.assertEquals(true, gHappy.gHappy("gg"));
+    }
+
+    @Test
+    public void testDoubleGEnd() {
+        Assertions.assertEquals(true, gHappy.gHappy("agg"));
+    }
+
+    @Test
+    public void testDoubleGBegin() {
+        Assertions.assertEquals(true, gHappy.gHappy("gga"));
+    }
+
+    @Test
+    public void testDoubleGMiddle() {
+        Assertions.assertEquals(true, gHappy.gHappy("abgga"));
+    }
+
+    @Test
+    public void testDoubleBeginAndSingleMiddle() {
+        Assertions.assertEquals(false, gHappy.gHappy("ggaga"));
+    }
+
+    @Test
+    public void testSingleMiddleAndDoubleMiddle() {
+        Assertions.assertEquals(false, gHappy.gHappy("agagga"));
+    }
+
+    @Test
+    public void testSingleBeginAndDoubleMiddle() {
+        Assertions.assertEquals(false, gHappy.gHappy("gagga"));
+    }
+
+    @Test
+    public void testFourGs() {
+        Assertions.assertEquals(true, gHappy.gHappy("gggg"));
+    }
 }
